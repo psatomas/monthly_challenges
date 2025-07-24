a337d482e6b1a24497f93c54ee8a0e84f5e57d8a
psatomas <psatomas@gmail.com>
2025-07-24 17:32:18 -0300

    feat: add january and february views and URLs

--- challenges/views.py
+++ challenges/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-    return HttpResponse("This works!")
+def january(request):
+    return HttpResponse("Eat no meat for the entire month!")
+
+def february(request):
+    return HttpResponse("Walk for at least 20 minutes every day!")
