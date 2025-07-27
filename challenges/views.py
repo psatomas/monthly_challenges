17f1083ee8bb71174ba5adeffb84035186829503
psatomas <psatomas@gmail.com>
2025-07-27 20:42:30 -0300

    feat: add monthly challenge HTML template and render logic

--- challenges/views.py
+++ challenges/views.py
@@ -47,8 +47,10 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenges_text = monthly_challenges[month]
-        response_data = f"<h1>{challenges_text}</h1>"
-        return HttpResponse(response_data)
+        return render(request, "challenges/challenge.html", {
+            "text": challenges_text,
+            "month_name": month.capitalize()
+        })
     except:
         return HttpResponseNotFound("<h1>This month is not supported!<h1>")
     
